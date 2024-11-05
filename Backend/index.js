aa9fb387cca0e8d918138b42786b8e7de4e4c965
alenvarghese12 <alenvarhannah@gmail.com>
2024-11-05 21:58:44 +0530

    deploy part 5

--- Backend/index.js
+++ Backend/index.js
@@ -97,7 +97,7 @@ const preventCache = (req, res, next) => {
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://waterway-booking2-1.onrender.com",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
